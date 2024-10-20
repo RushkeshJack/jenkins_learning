3d0515e10ff9ab5e2fbf004a4062ff46a8d76959
RushkeshJack <86036905+RushkeshJack@users.noreply.github.com>
2024-10-21 00:35:56 +0530

    updated print statement once again

--- hello_world.java
+++ hello_world.java
@@ -2,7 +2,7 @@ public class hello_world{
 
   public static void main(String[] args){
 
-       for(int i=-0;i<5;i++){
+       for(int i=-0;i<6;i++){
              System.out.println("Hello world");
         }
 
